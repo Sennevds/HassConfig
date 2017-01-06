ca05a5836f839b4605c590f24fd82c57d1a127de
Sennevds <senne.vandesompele@gmail.com>
2017-01-06 11:51:52 +0100

    changed script for lowercase

--- bin/check_config.sh
+++ bin/check_config.sh
@@ -3,7 +3,7 @@
 result=$(hass --config $TRAVIS_BUILD_DIR --script check_config)
 
 echo "$result"
-if [[ $result == *"ERROR"* ]]
+if [[ $result == *"ERROR"* ]] || [[ $result == *"error"* ]]
 then
   exit 1
 fi
