e31731c85e99fea6753a40a08b1dd216b8a4dfe6
Senne Vande Sompele <senne.vandesompele@gmail.com>
2017-03-16 11:05:58 +0100

    changed update scripts

--- bin/getcommits.sh
+++ bin/getcommits.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Become user 'hass'
-sudo su -s /bin/bash hass <<'EOF'
-# Activate the virtualenv
-source /srv/hass/hass_venv/bin/activate
 cd "/home/hass/.homeassistant"
 git fetch
 echo $(git rev-list --count master..origin/master)
 exit
-EOF
--- bin/pullconfig.sh
+++ bin/pullconfig.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Become user 'hass'
-sudo su -s /bin/bash hass <<'EOF'
-# Activate the virtualenv
-source /srv/hass/hass_venv/bin/activate
 cd "/home/hass/.homeassistant"
 git pull
 exit
-EOF
