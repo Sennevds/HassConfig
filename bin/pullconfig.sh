17c8c248b21555d04d15349651500c9c9d0d9f61
Senne Vande Sompele <senne.vandesompele@gmail.com>
2017-03-16 10:41:05 +0100

    changed update scripts

--- bin/pullconfig.sh
+++ bin/pullconfig.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd "/home/hass/.homeassistant/"
+# Become user 'hass'
+sudo su -s /bin/bash hass <<'EOF'
+# Activate the virtualenv
+source /srv/hass/hass_venv/bin/activate
+cd "/home/hass/.homeassistant"
 git pull
+exit
+EOF
