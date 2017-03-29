0069fc6f8ce5bcda5ff9dc88e3d8a922a86d2832
Senne Vande Sompele <senne.vandesompele@gmail.com>
2017-03-29 15:22:53 +0200

    new install

--- bin/getcommits.sh
+++ bin/getcommits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "/home/hass/.homeassistant"
+cd "/home/homeassistant/.homeassistant"
 git fetch
 echo $(git rev-list --count master..origin/master)
 exit
--- bin/pullconfig.sh
+++ bin/pullconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd "/home/hass/.homeassistant"
+cd "/home/homeassistant/.homeassistant"
 git pull
 exit
